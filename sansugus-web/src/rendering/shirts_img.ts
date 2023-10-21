70b1f430b0b68813681f133ad144e00a3cb38031
Juan Pereira <juan.cpereira@hotmail.com>
2023-10-21 09:53:39 +0200

    Update shirts_img.ts

--- sansugus-web/src/rendering/shirts_img.ts
+++ sansugus-web/src/rendering/shirts_img.ts
@@ -1,34 +1,26 @@
-import Migue from '../img/shirts/Migue.png'
-import VicoGK from '../img/shirts/Vico-GK.png'
-import Pepe from '../img/shirts/Pepe.png'
-import Dani from '../img/shirts/Dani.png'
-import Charly from '../img/shirts/Charly.png'
-import Fer from '../img/shirts/Fer.png'
-import Inigol from '../img/shirts/Inigol.png'
-import Joselu from '../img/shirts/Joselu.png'
-import Pere from '../img/shirts/Pere.png'
-import Portu from '../img/shirts/Portu.png'
-import Robert from '../img/shirts/Robert.png'
-import Vico from '../img/shirts/Vico.png'
-
-const players_img: { [key: string]: string } = {
-    "Luis Vico GK": VicoGK,
-    "Charly": Charly,
-    "Pepe": Pepe,
-    "Miguel Ángel Rodríguez": Migue,
-    "Daniel Sanz": Dani,
-    "Marcos Herrero":Dani, //Esta
-    "Sergio Hernández 'Checho'":Dani, //Esta
-    "Jorge Muñoz": Dani, //Esta
-    "Félix Barragán":Dani, //Esta
-    "Fernando Oteo":Fer,
-    "Iñigo Sáez Mesas": Inigol,
-    "Jose Delgado":Joselu,
-    "Juan Pereira":Pere,
-    "Javier Delgado":Portu,
-    "Roberto Lage":Robert,
-    "Luis Vico": Vico
-
+const imagePaths: { [key: string]: string } = {
+    "Luis Vico GK": 'Vico-GK.png',
+    "Charly": 'Charly.png',
+    "Pepe": 'Pepe.png',
+    "Miguel Ángel Rodríguez": 'Migue.png',
+    "Daniel Sanz": 'Dani.png',
+    "Marcos Herrero":'Mark.png', //Esta
+    "Sergio Hernández 'Checho'":'Checho.png', //Esta
+    "Jorge Muñoz": 'Drako.png', //Esta
+    "Félix Barragán":'Félix.png', //Esta
+    "Fernando Oteo":'Fer.png',//Esta
+    "Iñigo Sáez Mesas": 'Inigol.png',
+    "Jose Delgado":'Joselu.png',
+    "Juan Pereira":'Pere.png',
+    "Javier Delgado":'Portu.png',
+    "Roberto Lage":'Robert.png',
+    "Luis Vico": 'Vico.png'
+  };
+  
+  const players_img: { [key: string]: string } = {};
+  
+for (const player in imagePaths) {
+    players_img[player] = `src/img/shirts/${imagePaths[player]}`;
 }
 
 export function getShirtImage(name:string){
