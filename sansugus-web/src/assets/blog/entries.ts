35076a87d3058d24ddcd2c14ccb391fc6c874530
Juan Pereira <juan.cpereira@hotmail.com>
2023-12-16 00:27:13 +0100

    Fixed Footer Added Blog

---
+++ sansugus-web/src/assets/blog/entries.ts
@@ -0,0 +1,9 @@
+
+const dir = '/resources/img/blog/'
+export const entries = [
+    {
+        "title":"El enfrentamiento contra el Texas es la última bala que le queda a los nuestros",
+        "img":`${dir}Sansugus-VS-Texas.jpeg`,
+        "content":""
+    }
+]
