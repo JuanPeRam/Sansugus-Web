5eb81116747ec7bd059592bd68687ea50dd0e8c8
Juan Pereira <juan.cpereira@hotmail.com>
2023-12-21 00:14:38 +0100

    Starting grid

--- sansugus-web/src/components/home/Home.tsx
+++ sansugus-web/src/components/home/Home.tsx
@@ -1,10 +1,18 @@
+import {entries} from '../../assets/blog/entries'
+import '../../styles/Home.css'
 
 function Home() {
     
     return (
         <>
-            <h1>Bienvenido al sitio web oficial del <span>Sansugus FC</span></h1>
+            <h1 className='text-4xl'>Bienvenido al sitio web oficial del <span>Sansugus FC</span></h1>
             <h2 className="text-muted-foreground">Sitio en construcción...</h2>
+            <section className='grid-home'>
+                <article className='one rounded-xl'>Ver Jugadores</article>
+                <article className='two rounded-xl'>Ver Partidos</article>
+                <article className='three'>3</article>
+                <article className='four rounded-xl'>4</article>
+            </section>
         </>
     )
 }
