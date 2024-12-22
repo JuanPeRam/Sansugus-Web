c291547df63cc9ccc3c7efebf4ed7bde2d77a01b
Juan Pereira <juan.cpereira@hotmail.com>
2024-12-22 20:43:06 +0100

    Updated Palmares

--- sansugus-web/src/components/honors/Honors.tsx
+++ sansugus-web/src/components/honors/Honors.tsx
@@ -1,4 +1,4 @@
-import { CardBody, CardContainer } from "../ui/3d-card"
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card"
 
 export const Honors = () => {
 
@@ -6,6 +6,7 @@ export const Honors = () => {
         {
             name: 'Liga de Apertura Moralzarzal',
             year: '2024',
+            img: '/resources/img/trophys/laliga-trophy.png'
         },
     ]
     return (
@@ -13,9 +14,22 @@ export const Honors = () => {
             {honors.map((honor, index) => (
                 <CardContainer className="inter-var select-none" key={index}>
                     <CardBody className="relative group/card  hover:shadow-teamOrange hover:shadow-xl bg-black w-auto sm:w-[30rem] h-auto rounded-xl p-10 border flex flex-col justify-center items-center gap-2">
-                        <h1 className="text-2xl font-bold">{honor.name}</h1>
-                        <p>{honor.year}</p>
-                        <img src={'src/assets/photo/victoria.jpg'} alt="Foto del equipo" className="object-contain h-72 text-white" />
+                        <CardItem
+                            translateZ={50}
+                            className="text-2xl font-bold text-white">
+                            {honor.name}
+                        </CardItem>
+                        <CardItem
+                            as={'p'}
+                            className="text-white text-xl"
+                            translateZ={60}>
+                            {honor.year}
+                        </CardItem>
+                        <CardItem
+                            translateZ={100}
+                        >
+                            <img src={honor.img} alt="Foto del equipo" className="object-contain h-72 text-white" />
+                        </CardItem>
                     </CardBody>
                 </CardContainer>
             ))}
